385e85691e21ed72d3bfe83fb08b1d0f2a67cae2
Anton Kurilov <anton0kurilov@gmail.com>
2018-09-04 22:06:37 +0700

    Add function for open or close popup

--- md2.js
+++ md2.js
@@ -2,11 +2,22 @@ $(document).ready(function () {
     // when opening the sidebar
     $('.md2-sidebar-on').on('click', function () {
         $('#md2-sidebar').addClass('active');
+        $('.md2-popup').css({
+            display: 'none'
+        });
         $('.md2-overlay').fadeIn();
     });
     // if dismiss or overlay was clicked
     $('.md2-overlay').on('click', function () {
         $('#md2-sidebar').removeClass('active');
         $('.md2-overlay').fadeOut();
+    });
+    // open popup 
+    $('.md2-popup-open').on('click', function () {
+        $('.md2-overlay-popup').fadeIn();
+        $('.md2-overlay-popup').css({
+            display: 'flex'
+        });
+        $('.md2-popup').fadeIn();
     });
 });
